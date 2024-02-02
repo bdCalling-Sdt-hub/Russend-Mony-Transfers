34f12d38e7896cc72850ec8f2453a4594fb35ac2
naimulhassan2001 <developernaimul00@gmail.com>
2024-02-02 15:08:41 +0600

    client issues fixed

--- lib/global/api_url.dart
+++ lib/global/api_url.dart
@@ -13,8 +13,8 @@ class ApiUrl {
 
 
 
-  // static const String baseUrl = "http://russend.com" ;
-  static const String baseUrl = "http://192.168.10.18:3000" ;
+  static const String baseUrl = "http://russend.com" ;
+  // static const String baseUrl = "http://192.168.10.18:3000" ;
 
 
   static const String signUp = "$baseUrl/api/users/sign-up" ;
